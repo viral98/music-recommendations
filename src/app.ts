ed810ece98306463b18127da529e47081010e4e1
Viral Tagdiwala <tagdiwalaviral@gmail.com>
2022-03-12 14:59:23 -0800

    Song feature detection

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    FetchSongs()
+    const songData = FetchSongs()
     const ratings = [
         [1, 1, 1],
         [1, 0, 1],
