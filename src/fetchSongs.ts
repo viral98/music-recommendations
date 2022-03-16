0665b24a6ad8f395a8b10370946cfd2974289d61
Kevin Thomas <kevintab@tutanota.com>
2022-03-15 22:05:09 -0700

    split features into ranges

--- src/fetchSongs.ts
+++ src/fetchSongs.ts
@@ -13,9 +13,50 @@ export async function FetchSongs(){
   const result = await spotify.playlists.getPlaylist(DUMMY_PLAYLIST_ID);
 
   
-  const songData = await spotify.tracks.getAudioFeaturesForTracks(result.tracks.items.map((e) =>  e.track.id))
+  const songData = await spotify.tracks.getAudioFeaturesForTracks(
+    result.tracks.items.map((e) =>  e.track.id));
+  // Taking into consideration the following features from songs:
+  // 1. Danceability: Derived from combination of musical elements such as
+  //                  tempo, rhythm stability, beat strength, and overall
+  //                  regularity. Note that other selected features do not need
+  //                  to be musical elements since danceability score is derived
+  //                  from them. (Range: 0 - 1)
+  // 2. Energy: Perceptual features contributing to this attribute include
+  //            dynamic range, perceived loudness, timbre, onset rate, and
+  //            general entropy. (Range: 0 - 1)
+  // 3. Instrumentalness: Score predicting absence of vocals. (Range: 0 - 1)
+  // 4. Speechiness: Speechiness detects the presence of spoken words in a track.
+  //                 (Range: 0 - 1)
+  // 5. Valence: Positiveness conveyed in the track. Higher values indicate the
+  //             track is happy, cheerful, euphoric while lower values indicate
+  //             the track is negative e.g. sad, depressed, angry.
+  // Note that some of the selected attributes are either intentionally
+  // complementary or have similarities, this is to surface outliers and allow
+  // for an unbiased representation.
+  //
+  // See https://developer.spotify.com/documentation/web-api/reference/#/operations/get-several-audio-features
   const resultantMatrix:InputMatrix = songData.map((e)=>(
-    [e.acousticness, e.tempo, e.energy, e.liveness, e.loudness, e.speechiness]
-  ))
-  return resultantMatrix
+    [
+      Number(e.danceability <= 0.3), // Can't dance
+      Number(e.danceability > 0.3 && e.danceability <= 0.5), // Slow paced dance
+      Number(e.danceability > 0.5 && e.danceability <= 0.7), // Medium - high paced dance
+      Number(e.danceability > 0.7), // EDM etc.
+      Number(e.energy <= 0.3), // Calming, low energy
+      Number(e.energy > 0.3 && e.energy <= 0.5), // Medium-low
+      Number(e.energy > 0.5 && e.energy <= 0.7), // Medium-high
+      Number(e.energy > 0.7), // High energy
+      Number(e.instrumentalness <= 0.3), //  Maybe rap music or too wordy
+      Number(e.instrumentalness > 0.3 && e.instrumentalness <= 0.5), // Wordy parts interspersed with solo or instrumentals
+      Number(e.instrumentalness > 0.5 && e.instrumentalness <= 0.7), // 
+      Number(e.instrumentalness > 0.7), // Instrumental music
+      Number(e.speechiness <= 0.3), // Maybe instrumental
+      Number(e.speechiness > 0.3 && e.speechiness <= 0.5), // Few rap parts
+      Number(e.speechiness > 0.5 && e.speechiness <= 0.7), // Lots of spoken words
+      Number(e.speechiness > 0.7), // Rap music
+      Number(e.valence <= -0.5), // Depressed, Angry
+      Number(e.valence > -0.5 && e.valence <= 1), // Sad
+      Number(e.valence > 1 && e.valence <= 0.5), // Cheerful
+      Number(e.valence > 0.5)] // Euphoric
+  ));
+  return resultantMatrix;
 }
