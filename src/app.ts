9a7bb6f073418f115f9cfb0fd2532bc0b23c941d
Viral Tagdiwala <tagdiwalaviral@gmail.com>
2022-03-17 09:36:56 -0700

    Remove debug log

--- src/app.ts
+++ src/app.ts
@@ -5,8 +5,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', async (req, res) => {
-    const songData = await FetchSongs()  
-    console.log(songData);
+    const songData = await FetchSongs()
     const result = collaborativeFilter(songData, 2);
     res.send(result);
 });
