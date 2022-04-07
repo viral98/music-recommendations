a050becb481a322d443eb29967a39da2d63c6bf8
Kevin Thomas <kevintab@tutanota.com>
2022-04-07 14:38:21 -0700

    Introduce cosine similarity library

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,8 @@ import { collaborativeFilter } from './cf';
 import { SpotifyWebApi } from 'spotify-web-api-ts';
 import { DUMMY_PLAYLIST_ID, SPOTIFY_TOKEN } from './constants';
 import { fetchPlaylists } from './fetchPlaylists';
+const similarity = require( 'compute-cosine-similarity' );
+
 const app = express();
 const port = 3000;
 
@@ -15,6 +17,27 @@ app.get('/', async (req, res) => {
   // const playListData = await spotify.playlists.getPlaylist(DUMMY_PLAYLIST_ID[0]);
   // const commonLikings = playListData.tracks.items.map((e) => e.track.name).filter((value, index) => result.includes(index))
   res.send(result);
+  const listOfRecommendationsWithAttributes = (
+    Array.from(
+      { length: 10 },
+      () => Array.from(
+        { length: 20 },
+        () => parseFloat((Math.random()).toFixed(4))
+      )
+    )
+  );
+  let cosineSimilaritiesOfAllReccomendations = 0;
+  for (let i = 0; i < 10; i++) {
+    for (let j = i + 1; j < 10; j++) {
+      if (j == 10) continue;
+      cosineSimilaritiesOfAllReccomendations += similarity(
+        listOfRecommendationsWithAttributes[i],
+        listOfRecommendationsWithAttributes[j]
+      );
+    }
+  }
+  const averageCosineSimilarity = cosineSimilaritiesOfAllReccomendations / 45;
+  console.log(`Intra-list similarity is ${averageCosineSimilarity}`);
 });
 
 app.listen(port, () => {
